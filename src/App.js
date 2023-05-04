4f673721f8c46ee95e748680480f229a47c2a358
ycy <cksdyd93@gmail.com>
2023-05-04 10:20:26 +0900

    1±³½Ã state  - Ŭ·¡½ºÇü ÄÄÆ÷³ÍƮÀÇ state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import MyComponent from './MyComponent';
+import Counter from './Counter';
 
 const App = () => {
   return (
-    <MyComponent name="react">Hello React</MyComponent>
+    // <MyComponent name="react">Hello React</MyComponent>
+    <Counter />
   );
 };
 
