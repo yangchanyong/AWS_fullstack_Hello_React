3da507831c1fb6da3c9eae014fe3c092639b7025
cksdydsla93@gmail.com <cksdydsla93@gmail.com>
2023-05-08 11:34:49 +0900

    2교시

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
-    state = {message: ""};
+    state = {message: "", username:""};
+    // constructor(props) {
+    //     super(props);
+    //     this.handleChange = this.handleChange.bind(this);
+    //     this.handleClick = this.handleClick.bind(this);
+    // }
+
+    handleChange = (e) => {
+        this.setState({
+        [e.target.name]: e.target.value,
+        });
+    };
+
+    handleClick = () => {
+        alert(this.state.username + ":" + this.state.message);
+        this.setState({message:""});
+    };
+
+    handleKeyPress = (e) => {
+        if(e.key === "Enter") {
+            this.handleClick();
+        }
+    };
+
     render() {
         return (
             <div>
                 <h1>이벤트 연습</h1>
                <input 
+                name="username" 
+                placeholder="아무거나 입력해보세요" 
+                value={this.state.username}
+                onChange={this.handleChange}
+                onKeyPress={this.handleKeyPress}
+                /> 
+               <input 
                 name="message" 
                 placeholder="아무거나 입력해보세요" 
                 value={this.state.message}
-                onChange={(e) => {
-                    this.setState({
-                        message: e.target.value,
-                    });
-                }}
-            /> 
-            <button onClick={() => {
-                alert(this.state.message);
-                this.setState({message:""});
-            }} 
+                onChange={this.handleChange}
+                /> 
+            <button onClick={this.handleClick}
             >
                 확인
             </button>
